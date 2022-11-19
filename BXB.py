a0fceba2c278b74fbdb8cae2433792c682ce2626
AFGHAN HACK <110977192+AfghanHack001@users.noreply.github.com>
2022-11-19 11:43:10 +0430

    Update BXB.py

--- BXB.py
+++ BXB.py
@@ -1,5 +1,4 @@
-if 'aarch' in arch:
-    arch = 'aarch'
-    print('\033[1;92mCongratulatings! Your Deviec Support This Tools')
-    import NiCePiCDear
+import NiCePiCDear
+print('\033[1;92mCongratulatings! Your Deviec Support This Tools')
+
     
